15e4eb67f3ce40305e66429c3b9e2ee33356b4bc
NicholasLambert03 <Nicholas@spunow.co.uk>
2024-03-25 13:58:50 +0000

    bugfix

--- jessdlescript.js
+++ jessdlescript.js
@@ -129,7 +129,7 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = BIRTHWORDS[Math.floor(Math.random() * BIRTHWORDS.length)]
+let rightGuessString = BIRTHDAYWORDS[Math.floor(Math.random() * BIRTHDAYWORDS.length)]
 console.log("hey")
 document.addEventListener("keyup", (e) => {
 
